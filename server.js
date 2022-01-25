e675a22bd63689a86664a48f10deefc5a260719c
A7t5i0 <84842351+A7t5i0@users.noreply.github.com>
2022-01-25 18:41:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var app = express();
 
 app.use(express.static('build'));
 
-app.use('/css', express.static(__dirname + '/build/statoc/css'));
-app.use('/js', express.static(__dirname + '/build/static/js'));
-app.use('/images', express.static(__dirname + '/build/static/images'));
+app.use(express.static(__dirname + '/build/static));
 
 var server = app.listen(8081, () => {
     var port = server.address().port;
