85fac6671ac10b709964f9bd1f524b60e127ba90
A7t5i0 <84842351+A7t5i0@users.noreply.github.com>
2022-01-25 18:44:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var app = express();
 
 app.use(express.static('build'));
 
-app.use(express.static(__dirname + '/build/static));
-
 var server = app.listen(8081, () => {
     var port = server.address().port;
     console.log("server started at http://localhost:%s", port);
